520eea5e0e19feab1d575ae2f89dc71af5a068e4
akhilesh guleria <developakhilesh01@gmail.com>
2026-06-02 16:46:43 +0530

    make dummydata and booksslice and bookdata

---
+++ src/store/bookSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { booksData } from "../utils/books.data.js";
+
+const bookSlice = createSlice({
+    name:"books",
+    initialState:booksData,
+    reducers:{
+        addBook:(state,action)=>{
+              state.push(action.payload);
+        }
+    }
+})
+
+export const {addBook} = bookSlice.actions;
+export default bookSlice.reducer;
